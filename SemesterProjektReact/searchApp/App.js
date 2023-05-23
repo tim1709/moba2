15077dc7a08e51066c01c165aab8de473929b66c
Tim <60145752+tim1709@users.noreply.github.com>
2023-05-23 15:05:30 +0200

    Delete SemesterProjektReact directory

--- SemesterProjektReact/searchApp/App.js
+++
@@ -1,6 +0,0 @@
-import MainStackNavigator from "./src/navigation/MainStackNavigator";
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import Home from "./src/screens/Home";
-export default function App() {
-	return (<Home/>);
-}
