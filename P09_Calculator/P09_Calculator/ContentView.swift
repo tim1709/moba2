4968ac6e106117953c59600dd7bd34731ce2775b
damdiste <79465265+damdiste@users.noreply.github.com>
2023-05-23 09:01:33 +0200

    Refactored P9

---
+++ P09_Calculator/P09_Calculator/ContentView.swift
@@ -0,0 +1,64 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State var firstValue: String = "1"
+    @State var secondValue: String = "1"
+    @State var result: String = "2"
+    var body: some View {
+        VStack {
+            Text("Calculater").font(Font.title)
+            TextField("Enter first value", text: $firstValue).textFieldStyle(RoundedBorderTextFieldStyle()).padding(10).keyboardType(.decimalPad)
+            HStack{
+                Spacer()
+                CalcButton(text: "+", firstV: $firstValue, secondV: $secondValue, res: $result)
+                Spacer()
+                CalcButton(text: "-", firstV: $firstValue, secondV: $secondValue, res: $result)
+                Spacer()
+                CalcButton(text: "*", firstV: $firstValue, secondV: $secondValue, res: $result)
+                Spacer()
+                CalcButton(text: "/", firstV: $firstValue, secondV: $secondValue, res: $result)
+                Spacer()
+            }.padding(.vertical, 40)
+            TextField("Enter second value", text: $secondValue).textFieldStyle(RoundedBorderTextFieldStyle()).padding(10).keyboardType(.decimalPad)
+            Text(String(result)).font(Font.title)
+        }.padding()
+    }
+}
+
+struct CalcButton: View{
+    var text: String
+    @Binding var firstV: String
+    @Binding var secondV: String
+    @Binding var res: String
+    var body: some View {
+        Button(action: {calculate(text)}){Text(text).padding(20).border(Color.blue)}
+    }
+    
+    func calculate(_ op: String){
+        guard let fv = Double(firstV), let sv = Double(secondV) else {
+                        res = "Invalid Value"
+                        return}
+        switch op {
+            case "+":
+                res = String(fv + sv)
+            case "-":
+                res = String(fv - sv)
+            case "*":
+                res = String(fv * sv)
+            case "/":
+                if sv != 0 {
+                    res = String(fv / sv)
+                } else {
+                    res = "Division through 0 not possible"
+                }
+            default:
+                res = "Invalid Operator"
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P09_Calculator/P09_Calculator/P09_CalculatorApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct P09_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
